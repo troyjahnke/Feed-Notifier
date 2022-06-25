45133858bcb2fd2c337c17d03cf31ecb23996843
Troy Jahnke <troy@jahnke.dev>
2022-06-25 17:52:35 -0600

    Add dynamodb logic to scan items and perform feed lookup.

--- feednotifier.go
+++ feednotifier.go
@@ -2,27 +2,65 @@ package main
 
 import (
 	"context"
+	"log"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/mmcdole/gofeed"
 )
 
+type Feed struct {
+	Name   string `dynamodbav:"name"`
+	Url    string `dynamodbav:"url"`
+	Latest string `dynamodbav:"latest"`
+}
+
 func HandleRequest(ctx context.Context) {
-	print("hello world")
 	cfg, _ := config.LoadDefaultConfig(ctx, func(options *config.LoadOptions) error {
+		options.Region = "us-east-1"
 		return nil
 	})
 	svc := dynamodb.NewFromConfig(cfg)
-	feeds, err := svc.Scan(ctx, &dynamodb.ScanInput{
-		TableName: aws.String("Feeds"),
+	scannedFeeds, err := svc.Scan(ctx, &dynamodb.ScanInput{
+		TableName: aws.String("feeds"),
 	})
 	if err != nil {
-		panic(err)
+		log.Fatalln(err.Error())
 	}
 
-	for feed := range feeds.Items {
-		print(feed)
+	var feeds []Feed
+
+	err = attributevalue.UnmarshalListOfMaps(scannedFeeds.Items, &feeds)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	fp := gofeed.NewParser()
+
+	for _, feed := range feeds {
+		parsedFeed, err := fp.ParseURL(feed.Url)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		latestLink := parsedFeed.Items[0].Link
+		if latestLink != feed.Latest {
+			log.Println(latestLink)
+			updateExpression := "SET latest = " + latestLink
+			_, err := svc.UpdateItem(ctx, &dynamodb.UpdateItemInput{
+				TableName: aws.String("feeds"),
+				Key: map[string]types.AttributeValue{
+					"id": &types.AttributeValueMemberS{Value: feed.Name},
+				},
+				UpdateExpression: &updateExpression,
+			})
+                        if err != nil{
+                                log.Fatalln(err.Error())
+                        }
+		}
 	}
 }
 
--- go.mod
+++ go.mod
@@ -6,15 +6,20 @@ require (
 	github.com/aws/aws-lambda-go v1.32.0
 	github.com/aws/aws-sdk-go-v2 v1.16.5
 	github.com/aws/aws-sdk-go-v2/config v1.8.3
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.9.4
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.15.7
+	github.com/mmcdole/gofeed v1.1.3
 )
 
 require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.1.0 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.4.3 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.6.0 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.12 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.6 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.2.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.13.7 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.6 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.3.2 // indirect
@@ -23,5 +28,11 @@ require (
 	github.com/aws/smithy-go v1.11.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
